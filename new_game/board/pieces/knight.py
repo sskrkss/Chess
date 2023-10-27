0c3c6767fb1552dc0ebd5771fbfaa822c31b0c2b
sskrk <shihov941@gmail.com>
2023-10-27 13:32:36 +0600

    v1.1 Добавлены ходы коня

--- new_game/board/pieces/knight.py
+++ new_game/board/pieces/knight.py
@@ -6,3 +6,9 @@ class Knight(Figure):
 
     def __repr__(self):
         return f'Knight({self.hor}, {self.ver})'
+
+    def possible_moves(self):
+        return [
+            (self.hor + i, self.ver + j) for j in range(-2, 3) for i in range(-2, 3)
+            if 0 <= self.hor + i <= 7 and 0 <= self.ver + j <= 7 and abs(i) + abs(j) == 3
+        ]
