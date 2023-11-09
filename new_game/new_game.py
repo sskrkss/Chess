de67f402229adc8e8a764e3b2080502cea1513d2
sskrk <shihov941@gmail.com>
2023-11-10 01:50:22 +0600

    v1.10 Реализован удобный интерфейс для хода с помощью задания координат

--- new_game/new_game.py
+++ new_game/new_game.py
@@ -2,9 +2,14 @@ from .chessrules import *
 
 
 class NewGame(ChessRules):
+    _dict_ver = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
+    _dict_hor = {'1': 7, '2': 6, '3': 5, '4': 4, '5': 3, '6': 2, '7': 1, '8': 0}
     def __init__(self):
         super().__init__()
         self.game_status = 'in progress'
+
+    def converter(self, coord):
+        return self._dict_hor[coord[1]], self._dict_ver[coord[0]]
 
     def new_game(self):
         while True:
@@ -12,7 +17,8 @@ class NewGame(ChessRules):
             print()
             print('Ход белых')
             print()
-            h1, v1, h2, v2 = [int(input()) for _ in range(4)]
+            h1, v1 = self.converter(input().lower())
+            h2, v2 = self.converter(input().lower())
             if not self.move(h1, v1, h2, v2, 'w'):
                 break
         while True:
@@ -20,6 +26,7 @@ class NewGame(ChessRules):
             print()
             print('Ход черных')
             print()
-            h1, v1, h2, v2 = [int(input()) for _ in range(4)]
+            h1, v1 = self.converter(input().lower())
+            h2, v2 = self.converter(input().lower())
             if not self.move(h1, v1, h2, v2, 'b'):
                 break
